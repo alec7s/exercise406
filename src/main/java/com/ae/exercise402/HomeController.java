e4c00d2dd12abb849c14ba2088ef09009a89858f
Alex Evans <alex.c.evans1@gmail.com>
2020-04-06 21:30:46 -0400

    Initial commit

---
+++ src/main/java/com/ae/exercise402/HomeController.java
@@ -0,0 +1,12 @@
+package com.ae.exercise402;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
