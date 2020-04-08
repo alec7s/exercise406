3d0993d1b5b3c8489d186fdef27a934d739abe16
Alex Evans <alex.c.evans1@gmail.com>
2020-04-08 18:54:10 -0400

    completed

--- src/main/java/com/ae/exercise402/SecurityConfiguration.java
+++ src/main/java/com/ae/exercise402/SecurityConfiguration.java
@@ -18,7 +18,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().anyRequest().authenticated().and().formLogin();
+                .authorizeRequests().anyRequest().authenticated().and().formLogin()
+        .loginPage("/login").permitAll();
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
